a7107c152338d31e92ba6022704df0df16a978c7
vika <viktoriia.schmidt@gmx.de>
2023-07-26 15:55:13 +0200

    Added github_api tests

---
+++ modules/api/clients/github.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class GitHub:
+    def get_user(self, username):
+        r = requests.get(f"https://api.github.com/users/{username}")
+        body = r.json()
+
+    def search_repo(self, name):
+        r = requests.get(
+            "https://api.github.com/search/repositories", params={"q": name}
+        )
+        body = r.json()
+
+        return body
